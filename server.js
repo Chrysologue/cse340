2f90edb218bc7e042b8bf1559ef16bf966cc876f
Chryso <chrysoBeatz@gmail.com>
2025-08-01 13:45:50 +0000

    defining port

--- server.js
+++ server.js
@@ -33,12 +33,11 @@ app.get("/", (req, res) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 8080
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on:${port}`)
 })
